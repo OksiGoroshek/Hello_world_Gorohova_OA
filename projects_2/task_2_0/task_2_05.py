551ef17f5f51aaf6a302ae063f471687f106ac71
Gorohova Oksana <Goroxova.oks@yandex.ru>
2026-02-08 23:14:28 +0300

    Add files via upload

---
+++ projects_2/task_2_0/task_2_05.py
@@ -0,0 +1,4 @@
+name = 'Вася'
+group = 2
+score = 100
+print(f'Студент {name} из группы {group} получил {score} баллов')
---
+++ projects_2/task_2_0/task_2_06.py
@@ -0,0 +1,3 @@
+f = open('C:/Users/Оксана/Documents/gorohova_oa/projects_2/task_2_0/output.txt', 'w', encoding='utf-8')
+print('Меня зовут Горохова Оксана', file=f)
+f.close()
